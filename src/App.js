6a4bd3ee2128f2b1d2e711bba8b7c65672683576
Darwin <risser.darwin@gmail.com>
2019-03-13 13:58:28 -0500

    init homepage setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+        <header id="header">
+          <div id="homeHeader">
+            <div id="leftSide">Welcome to TeacherFinder!</div>
+            <div id="rightSide"><span>Home</span><span>My Account</span></div>
+          </div>
         </header>
+        <main id="homeContent">
+          <div id="homeLeftSide">
+            <h1>Directors</h1>
+            <p>Already have an account?</p>
+            <button className="login">Login</button>
+          </div>
+          <div id="homeRightSide">
+            <h1>Teachers</h1>
+            <p>Already have an account?</p>
+            <button className="login">Login</button>
+          </div>
+        </main>
       </div>
     );
   }
